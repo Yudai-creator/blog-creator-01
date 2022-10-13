64bd266b2916a30ee838a25d87a3c15377eae17a
Yudai-creator <yudaipx77@gmail.com>
2022-10-13 11:56:48 -0400

    blog creator first commit

---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hero_container">
+    <header class="w-full flex justify-between items-center">
+      <img class="logo" src="https://ik.imagekit.io/u33i3sss0/Portfolio_Website/android-chrome-512x512_exmBxaGLK.png?ik-sdk-version=javascript-1.4.3&updatedAt=1665675785139" alt="logo">
+      <!-- <nav>
+        <a href="#">About me</a>
+        <a href="#">Contact</a>
+      </nav> -->
+      <a class="p-4 bg-gray-400 rounded-full" href="https://yudaicreator.com">Portfolio</a>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style>
+
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body{
+    background-color: #010713;
+    color: #DAE6FF;
+
+    padding: 2rem;
+  }
+
+  .logo{
+    max-width: 5rem;
+  }
+  
+</style>
